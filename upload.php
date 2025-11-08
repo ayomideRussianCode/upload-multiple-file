81231bf2e001a377822af2e49332865ba84c79af
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-11-08 04:12:04 +0100

    added comments for clearer understanding

--- upload.php
+++ upload.php
@@ -4,6 +4,7 @@ if($_SERVER ['REQUEST_METHOD'] == 'POST'){
 
        $upload_dir = "uploads/";
 
+       // creates uploads directory
        if(!is_dir ($upload_dir)){
         mkdir($upload_dir, 0777, true);
        }
@@ -17,14 +18,16 @@ foreach($_FILES['files']['name'] as $key => $file_name){
 
         //upload-err-ok instead of 0
         if($file_error === UPLOAD_ERR_OK){
+                // checks for size
 
                 if($file_size > 5 * 1024 * 1024){
                         echo "Error too big";
                 }
-
+                 //checks for file type
                 elseif(!in_array($file_type, ['jpg', 'jpeg', 'png', 'gif', 'pdf'])){
                         echo "Error file type  $file_name is not allowed";
                 }
+                //checks for upload
                 else{
                         if(move_uploaded_file($file_tmp, $target_file)){
                                 echo "File:  {$file_name} uploaded successfully <br> <br>";
@@ -32,18 +35,13 @@ foreach($_FILES['files']['name'] as $key => $file_name){
                                 echo "Error: There was a time issue uploading $file_name <br>";
                         }
                 }
+                //checking for errors
+        }else{
+                echo "Error : File $file_name failed to upload due to error $file_error <br> <br>";
         }
-
 }
 
-
-
-
-
-
-
-        // echo "<pre>";
-        // var_dump($file_tmp);
-        // echo "</pre>";
-
+// for loop
+}else {
+        echo "No files were uploaded";
 }
